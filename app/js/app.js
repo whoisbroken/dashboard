7a86f54555c37904972145d24b16a880f41e1e9a
Alexander Petriga <cazhqa666@gmail.com>
2020-10-21 18:34:35 +0300

    init project/add sidebar

---
+++ app/js/app.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // this function runs when the DOM is ready, i.e. when the document has been parsed
+
+  // all js code should go below this line
+});
